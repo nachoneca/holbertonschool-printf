fefe512d25245997419651f0a3bf85008fae55a0
nachoneca <8666@holbertonstudents.com>
2024-03-19 16:56:26 +0000

    fixed??

--- _printf.c
+++ _printf.c
@@ -4,8 +4,10 @@
 * _printf - produces output according to a format.
 * @format: Pointer to a charstring to a constant char.
 * Return: number of characters printed
+*
 * (excluding the null byte used to end output to strings)
 */
+
 int _printf(const char *format, ...)
 {
         int count = 0;
