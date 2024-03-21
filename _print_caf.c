e8b462aad10e59852441bb7e92d773e60522db16
root <alexisoblivion@gmail.com.com>
2024-03-21 03:15:05 +0000

    Description fixed

--- _print_caf.c
+++ _print_caf.c
@@ -1,9 +1,14 @@
 #include"main.h"
-/*
-*_print_char_and_format - print cgar and format.
-*@format_char: char given
-*@count: contador
-*/
+
+/**
+ * _print_char_and_format - This function prints a
+ * character that represents the format specifier
+ * and the format character itself, while also
+ * incrementing the count by 2.
+ * @format_char: The character that represents the format specifier.
+ * @count: A pointer to an integer that stores the count of characters
+ * printed.
+ */
 void _print_char_and_format(char format_char, int *count)
 {
 	_putchar('%');
--- _print_string.c
+++ _print_string.c
@@ -1,8 +1,16 @@
 #include"main.h"
-/*
- *_print_string - to print string into the main function
- *@str: string to print
- *@count: contador
+/**
+ * _print_string - A function that prints a string followed by a new line.
+ * @str: A pointer to the string to be printed.
+ * @count: A pointer to an integer that keeps
+ * track of the number of characters printed.
+ *
+ * This function checks if the string is NULL,
+ * and if so, it prints "(null)" and updates
+ * the count variable to 6 (the length of "(null)").
+ * If the string is not NULL, it
+ * iterates through each character in the string,
+ * printing it and incrementing the count variable by 1.
  */
 void _print_string(char *str, int *count)
 {
